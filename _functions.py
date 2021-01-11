abdf166a228c1ef56e1d8fad36cd5594dc61c42b
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2021-01-11 22:03:33 +0800

    new styling option and glitch name

--- _functions.py
+++ _functions.py
@@ -12,10 +12,35 @@ def randomString(stringLength):
 	lettersAndDigits = string.ascii_letters + string.digits
 	return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
 
-def gen_names(prefix,count):
+def gen_names(prefix,count,style,longname):
     names = []
+    characters = list(string.ascii_uppercase+string.ascii_lowercase)
+    digits = list(string.digits)
     for i in range(0,count):
+        if longname == True:
+            prefix = ''.join(random.sample(prefix,len(prefix)))
         names.append(prefix+'_'+randomString(5))
+    if style == True:
+        for name in names:
+            new_name = ''
+            c_no = [156,312,260]
+            c_offset = random.choice(c_no)
+            while c_offset in c_no:
+                c_offset = random.randint(1,12) * 52
+
+            d_offset = random.randint(1,4) * 10
+            for c in [char for char in name]:
+                if c in characters:
+                    new = characters.index(c) + 119808 + c_offset
+                    new = chr(new)
+                elif c in digits:
+                    new = digits.index(c) + 120782 + d_offset
+                    new = chr(new)
+                else:
+                    new = c
+                new_name = new_name + new
+            index = names.index(name)
+            names[index] = new_name
     return names
 
 def wait():
